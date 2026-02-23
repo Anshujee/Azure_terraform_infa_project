62ba8c848525c4a294c8c4be4e25b9647ecab824
Anshu <jeeanshu24@gmail.com>
2026-02-23 23:36:38 +0530

    change Pub_ key for CI/Cd to run

--- main.tf
+++ main.tf
@@ -111,7 +111,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
 
   admin_ssh_key {
     username   = var.admin_username
-    public_key = file("/Users/anshujee/.ssh/id_rsa.pub")
+    public_key = file("ssh_key.pub")
   }
 
   os_disk {
